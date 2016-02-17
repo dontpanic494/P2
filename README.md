6ec27e40019aa6832c8ca2d606ac368d573f9e64
John Lincoln <jlincoln88@gmail.com>
2016-02-17 16:24:21 -0500

    added readme and css

---
+++ README.md
@@ -0,0 +1,19 @@
+# P2
+
+## Live URL
+TBD
+
+## Description
+TBD
+
+## Demo
+TBD
+
+## Details for teaching team
+TBD
+
+## Outside code
+TBD
+
+## Comments
+TBD
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<meta charset='utf-8'>
+		<title>Password Generator</title>
+
+		<link rel='stylesheet' href='main.css'>
+		<?php require('logic.php'); ?>
+
+	</head>
+
+	<body>
+		<h1>Temp</h1>
+
+		
+	</body>
+
+</html>
