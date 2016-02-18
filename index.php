69cc8438bfa4ef0f181f08658b10fd76ba06cb49
John Lincoln <jlincoln88@gmail.com>
2016-02-17 19:54:49 -0500

    Premade word list and word count operating

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
+<?php
+error_reporting(-1); # Report all PHP errors
+ini_set('display_errors', 1);
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		
 		<meta charset='utf-8'>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Password Generator</title>
 
-		<link rel='stylesheet' href='main.css'>
-		<?php require('logic.php'); ?>
+		<link rel='stylesheet' href='CSS/main.css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<?php require'logic.php'; ?>
 
 	</head>
 
 	<body>
-		<h1>Temp</h1>
+		<div class='container'>
+			<h1>Password Generator</h1>
+			<h2 class="generated-password">
+				<?php 
+				for($x=0; $x<$wordCount; $x++){
+					$select=array_rand($words);
+					echo $words[$select];
+				}
+				?>
+			</h2>
 
-		
+
+			<form action='index.php' method='GET'>
+				<input type='text' id="num-word-input" class='form-control' name='word_count' placeholder='4'>Number of Words
+				<br>
+      			<input type='checkbox' name='add_num'> Add Number
+   				<input type='checkbox' name='add_spe'> Add Special
+   				<br>
+   				<input type='submit' class='btn btn-default' value='Generate'>
+			</form>
+		</div>
+
 	</body>
 
 </html>
--- logic.php
+++ logic.php
@@ -1,5 +1,49 @@
 <?php
 
-#code here
+
+
+$formData = $_GET;
+
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+
+$wordCount = 4;
+
+if (!empty($formData['word_count'])) {
+	$wordCount = intval($formData['word_count']);
+}
+/*
+$addNum = !empty($formData['add_num']);
+$addSpe = !empty($formData['add_spe']);
+
+if ($addNum) {
+
+}
+
+$test = file_get_contents('http://www.paulnoll.com/Books/Clear-English/words-11-12-hundred.html');
+$wordsArray = preg_match_all('#<li>(.+?)</li>#is', $test,$out);
+
+//echo var_dump($out);
+*/
+$words = [];
+
+$words[0] = 'First';
+$words[1] = 'Second';
+$words[2] = 'Third';
+$words[3] = 'Forth';
+$words[4] = 'Fifth';
+$words[5] = 'Sixth';
+$words[6] = 'Seventh';
+$words[7] = 'Eighth';
+$words[8] = 'Ninth';
+$words[9] = 'Tenth';
+$words[10] = 'Eleventh';
+$words[11] = 'Twelveth';
+$words[12] = 'Thirteenth';
+$words[13] = 'Fourteenth';
+$words[14] = 'Fifteenth';
+
+
 
 ?>
